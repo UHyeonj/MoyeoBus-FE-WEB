2d5e57aa4cfc9652257d1ef0cd994bf8f1687467
UHyeonj <dngusdlqwkd@gmail.com>
2025-11-19 22:26:53 +0900

    fix: 파일명 변경

---
+++ src/types/localType.ts
@@ -0,0 +1,76 @@
+//기본 통신 구조
+export interface ApiResponse<T> {
+  code: string;
+  message: string;
+  result: T;
+  isSuccess: boolean;
+}
+
+//지자체 구역별 이용 현황
+export interface RegionItem {
+  areaId: number;
+  sigunguName: string;
+  count: number;
+  ratio: string;
+}
+
+export interface LocalUsageResult {
+  localName: string;
+  items: RegionItem[];
+}
+
+//월별 인원 통계
+export interface MonthlyItem {
+  date: string;
+  useCount: number;
+}
+
+export interface LocalMonthlyUsageResult {
+  govName: string;
+  data: MonthlyItem[];
+}
+
+//설문 통계
+export interface SurveyItem {
+  reason: string;
+  count: number;
+  ratio: string;
+}
+
+export interface LocalSurveyResult {
+  items: SurveyItem[];
+}
+
+//시간대별 통계 현황
+export interface HourlyItem {
+  hour: number;
+  useCount: number;
+}
+
+export interface LocalHourlyUsageResult {
+  govName: string;
+  data: HourlyItem[];
+}
+
+//지자체 노선별 통계 조회
+export interface RouteItem {
+  routeId: number;
+  stationCount: number;
+  peopleCount: number;
+  distance: number;
+}
+
+export interface LocalRouteUsageResult {
+  govName: string;
+  items: RouteItem[];
+}
+
+//지자체 노선별 목적지, 출발지 기준 통계
+export interface DestinationItem {
+  stationName: string;
+  count: number;
+}
+
+export interface LocalRouteDestinationResult {
+  items: DestinationItem[];
+}
